eb9a6381a020589df64f663329671fc9e9d0cebb
Pablo Zdrojewski <pablo.zd@gmail.com>
2022-05-17 12:21:45 -0300

    gramatica test.py arreglada

--- Clase-07/ParserMinimalistaStablerPython/test.py
+++ Clase-07/ParserMinimalistaStablerPython/test.py
@@ -1,3 +1,11 @@
-g = [([casa], [('cat','N')]),([la], [('sel','N'),('cat','D'),('neg','case')]),
-    ([pinta],[('sel','D'),('pos','case'),('cat','V')]),
+g = [(['pinta'],[('sel','D'),('cat','V'),('neg','h')]),
+        (['casa'],[('cat','N')]),
+        (['la'],[('sel','N'),('cat','D'),('neg','ac')]),
+        (['Ivan'],[('cat','D'),('neg','case')]),
+        ([ ],[('sel','V'),('pos','ac'),('sel','D'),('cat','v')]),     
+        ([ ],[('sel','v'),('pos','h'),('pos','case'),('cat','T')]),
+        ([ ],[('sel','T'),('cat','C')]),
 ]
+
+
+# El rasgo ('neg','V') ('pos','V') es para movimiento de remanente. 
